f86ced65240d440ffdd3a8b63fe01962d6d257f3
neider <neider@email.com>
2026-01-25 16:40:34 -0500

    ready to deploy v3

--- config/settings.py
+++ config/settings.py
@@ -121,7 +121,7 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': dt.timedelta(days=90),
     'REFRESH_TOKEN_LIFETIME': dt.timedelta(days=91),
-    "TOKEN_OBTAIN_SERIALIZER": "apps.login.serializers.CustomTokenObtainPairSerializer",
+    #"TOKEN_OBTAIN_SERIALIZER": "apps.login.serializers.CustomTokenObtainPairSerializer",
 }
 
 MIDDLEWARE = [
