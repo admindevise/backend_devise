8f87991c9f39d46b44856d1a0737b59d20462e2a
neider <neider@email.com>
2026-01-26 15:31:44 -0500

    ready to deploy v8

--- build.sh
+++ build.sh
@@ -8,13 +8,13 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 # Initialize audit actions
-python manage.py init_audit_actions
+#python manage.py init_audit_actions
+python manage.py cities_light --force-all
+#python manage.py import_druo_data --all
 
-python manage.py import_druo_data --all
 
-
-DJANGO_SUPERUSER_USERNAME=admin \
-DJANGO_SUPERUSER_EMAIL=admin@admin.com \
-DJANGO_SUPERUSER_PASSWORD=admin \
-DJANGO_SUPERUSER_PHONE=3227177889 \
-python manage.py createsuperuser --noinput || true
+#DJANGO_SUPERUSER_USERNAME=admin \
+#DJANGO_SUPERUSER_EMAIL=admin@admin.com \
+#DJANGO_SUPERUSER_PASSWORD=admin \
+#DJANGO_SUPERUSER_PHONE=3227177889 \
+#python manage.py createsuperuser --noinput || true
